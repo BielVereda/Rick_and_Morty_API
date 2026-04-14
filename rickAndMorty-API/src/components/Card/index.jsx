dc35066cc31ec05e08230b7c08b95a16649790f2
BielVereda <gabrielsantosvereda@gmail.com>
2026-04-14 10:42:00 -0300

    feat: add Card jsx and css

--- rickAndMorty-API/src/components/Card/index.jsx
+++ rickAndMorty-API/src/components/Card/index.jsx
@@ -1,13 +1,32 @@
-import axios from "axios";
 import "./style.css";
 
-export const Card = () => {
+import Specie from "./../../assets/images/icons/alienIcon.png"
+import Alive from "./../../assets/images/icons/aliveIcon.png"
+import Planet from "./../../assets/images/icons/planetIcon.png"
 
-    axios.get("https://rickandmortyapi.com/api/character")
-        .then(res => console.log(res.data.results))
-        .catch(err => console.log(err));
+export const Card = ({image, name, specie, status, location}) => {
+
 
     return (
-        <h1>Card</h1>
+        <div className="card-characters">
+            <img src={image} alt="Person Image" />
+            <h3>{name}</h3>
+            <div className="description-characters">
+                <ul>
+                    <li>
+                        <img src={Specie} alt="Specie" />
+                        {specie}
+                    </li>
+                    <li>
+                        <img src={Alive} alt="Status" />
+                        {status}
+                    </li>
+                    <li>
+                        <img src={Planet} alt="Location" />
+                        {location}
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 };
